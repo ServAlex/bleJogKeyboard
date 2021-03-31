11e6ec7fdfc33ef629d2f45d8d0b4edbbbb44fdb
alex <servalex1@gmail.com>
2021-03-31 16:04:21 +0300

    updating voltage once every 5 seconds

--- SensorCotnroller.cpp
+++ SensorCotnroller.cpp
@@ -3,6 +3,7 @@
 
 float lastSentVoltage;
 float voltageDifferenceThreshold;
+int32_t lastSentTime;
 
 SensorController::SensorController(Logger* logger, SensorsModel* model)
 {
@@ -12,21 +13,24 @@ SensorController::SensorController(Logger* logger, SensorsModel* model)
     sensorsConfig.voltage_active = true;
     voltageDifferenceThreshold = 0.5;
     lastSentVoltage = 3.8;
+    lastSentTime = 0;
 
     sensorsSetup(sensorsConfig);
 }
 
 void SensorController::RegularUpdate()
 {
+    uint32_t time = millis();
     pollSensors();
 
     sensorsModel->voltage = battery_voltage;
     sensorsModel->batteryPercentage = battery_percentage;
 
-    if(abs(lastSentVoltage - sensorsModel->voltage)>voltageDifferenceThreshold)
+    if(abs(lastSentVoltage - sensorsModel->voltage)>voltageDifferenceThreshold || time - lastSentTime > 5000)
     {
         logger->Log("sent new voltage " + String(battery_voltage));
         lastSentVoltage = sensorsModel->voltage;
+        lastSentTime = time;
         stateChangeWatcher->VoltageChanged(sensorsModel->voltage, sensorsModel->batteryPercentage);
     }
 }
